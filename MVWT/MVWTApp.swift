607229e4437329dd497c572520a3f2f32f8f902d
Mike Velu <mikevelu@icloud.com>
2023-12-31 23:48:37 +0000

    initial commit

---
+++ MVWT/MVWTApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVWTApp.swift
+//  MVWT
+//
+//  Created by Mike Velu on 31/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct MVWTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
